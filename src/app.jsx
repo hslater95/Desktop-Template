2bd261b145afacdf75a0c6f2ef478047a37f471b
hslater95 <hasan.slater95@gmail.com>
2023-12-10 18:38:51 -0700

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import "./index.css";
+const root = createRoot(document.body);
+root.render(<h2>Desktop Template</h2>);
